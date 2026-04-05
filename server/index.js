30dee94edb23a4cd71c34e079cf1c5c7d36cca7f
Jose Ruano <ruano044@gmail.com>
2026-04-05 19:28:45 +0200

    Use PORT env variable for Railway

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const http = require("http");
 const db = require("./db");
 const { setupDashboard } = require("./dashboard");
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 const app = express();
 const server = http.createServer(app);
 setupDashboard(app);
